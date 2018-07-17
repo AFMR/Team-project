a8165cc5aa3bcc865642e4a908f5e92e0f35167d
Eury <32747825+AFMR@users.noreply.github.com>
2018-07-17 14:15:29 -0700

    Add files via upload

--- header.php
+++ header.php
@@ -1,8 +1,29 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset = "utf-8">
-<title>My Recipes</title>
-</head>
-        <P> The first try </p>
-<body>
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        link href="https://fonts.googleapis.com/css?family=Trocchi" rel="stylesheet"
+        <title>My Recipes</title>
+        <!-- ?php wp_enqueue_style( 'style', get_template_directory_uri() ); ?-->
+        <?php wp_head(); ?>
+        <!--IMPORTANT    Find a way to link CSS -->
+    </head>
+    <body>
+        <header class="header">
+            <div class="header-container">
+                <div id="header-main">
+                    <button id="social">Social media</button>
+                    <p id="header-title">Recipes for you</p>
+                    <button id="Subscribe">Subscribe</button>
+                </div>
+                <div class="header-links">
+                    <div id="welcome">Welcome</div>
+                    <div id="appetizers">Appetizers</div>
+                    <div id"entrees">Entrees</div>
+                    <div id="desserts">Desserts</div>
+                    <div id="tips">Tips</div>
+                    <div is="my-recipes">My recipes</div>
+                </div>
+            </div>
+        </header>
